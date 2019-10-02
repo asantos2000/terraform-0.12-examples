fe7e84aa768ce40c5efb740efc3814fb537b7dbe
Anderson Santos <adsantos@gmail.com>
2019-10-02 11:58:04 -0300

    error function file

--- new-template-syntax/main.tf
+++ new-template-syntax/main.tf
@@ -4,48 +4,63 @@ terraform {
 
 variable "names" {
   description = "list of names"
-  default = ["Peter", "Paul", "Mary"]
+  default     = ["Peter", "Paul", "Mary"]
 }
 
 variable "voter" {
   description = "name of voter"
-  default = "Roger"
+  default     = "Roger"
 }
 
 variable "candidate" {
   description = "name of candidate voter is voting for"
-  default = "Beto O'Rourke"
+  default     = "Beto O'Rourke"
 }
 
 data "template_file" "actual_vote" {
-  template = file("actual_vote.txt")
+  template = file("${path.cwd}/actual_vote.txt")
 
   vars = {
-    voter = var.voter
+    voter     = var.voter
     candidate = var.candidate
   }
+}
+
+output pathCWD {
+  value = "${path.cwd}"
+}
+
+output pathModule {
+  value = "${path.module}"
+}
+
+output pathRoot {
+  value = "${path.root}"
+}
+
+output workspace {
+  value = "${terraform.workspace}"
 }
 
 # This will work with Template Provider 2.0
 # But this does not work yet
-data "template_file" "rigged_vote" {
-  template = file("rigged_vote.txt")
+# data "template_file" "rigged_vote" {
+#   template = file("rigged_vote.txt")
 
-  vars = {
-    voter = var.voter
-    candidate = var.candidate
-  }
-}
+#   vars = {
+#     voter = var.voter
+#     candidate = var.candidate
+#   }
+# }
 
 # Note that the blank line is intentional
 # so that first name goes on its own line
 # Also note use of ~ to suppress newlines
 output all_names {
   value = <<EOT
-
-%{ for name in var.names ~}
-${name}
-%{ endfor ~}
+%{for name in var.names~}
+I'm voter, my name is ${name}
+%{endfor~}
 EOT
 }
 
@@ -54,9 +69,9 @@ EOT
 # as "just_mary ="
 output "just_mary" {
   value = <<EOT
-%{ for name in var.names ~}
-%{ if name == "Mary" }${name}%{ endif ~}
-%{ endfor ~}
+%{for name in var.names~}
+%{if name == "Mary"}${name}%{endif~}
+%{endfor~}
 EOT
 }
 
